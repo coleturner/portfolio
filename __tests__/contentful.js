38d002ade4c657c890c43836d9805bbd0dc15183
Cole Turner <turner.cole@gmail.com>
2017-06-21 23:10:38 -0400

    Add tests

---
+++ __tests__/contentful.js
@@ -0,0 +1,16 @@
+require('dotenv').config()
+
+it('has valid credentials', () => {
+  expect(process.env.CONTENTFUL_SPACE_ID).toBeDefined();
+  expect(process.env.CONTENTFUL_ACCESS_TOKEN).toBeDefined();
+
+  const contentful = require('contentful');
+  const client = contentful.createClient({
+    space: process.env.CONTENTFUL_SPACE_ID,
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+  });
+
+  client.getSpace().then(space => {
+    expect(space.sys).toBe({ type: 'Space', id: process.env.CONTENTFUL_SPACE_ID })
+  });
+});
