cdb4f2676a2d4698a78d0dac7550a3b95aea9d1b
Cole Turner <turner.cole@gmail.com>
2020-07-22 02:19:45 -0700

    update sitemap frequency

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -3,21 +3,29 @@ import { BASE_URL } from '../lib/constants';
 import { getAllPostsForSitemap } from '../lib/api';
 
 const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
-    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
         <url>
             <loc>${`${BASE_URL}`}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.5</priority>
         </url>
         <url>
             <loc>${`${BASE_URL}resume`}</loc>
+            <changefreq>monthly</changefreq>
+            <priority>0.5</priority>
         </url>
         <url>
             <loc>${`${BASE_URL}blog`}</loc>
+            <changefreq>daily</changefreq>
+            <priority>0.5</priority>
         </url>
         ${posts
           .map(({ slug }) => {
             return `
                     <url>
                         <loc>${`${BASE_URL}posts/${slug}`}</loc>
+                        <changefreq>daily</changefreq>
+                        <priority>0.7</priority>
                     </url>
                 `;
           })
