d0653d1de67391746edf415996a1e12bdd8755db
Cole Turner <turner.cole@gmail.com>
2020-07-25 11:53:16 -0700

    add meta charset tag

--- components/meta.js
+++ components/meta.js
@@ -9,7 +9,8 @@ export default function Meta() {
 
   return (
     <Head>
-      <title key="title">Cole Turner &middot; Software Engineer</title>
+      <meta charSet="utf-8" />
+      <title key="title">{title}}</title>
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1, shrink-to-fit=no"
