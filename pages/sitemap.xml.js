fe62e3bd59699d08165e05c432b60df26b60f888
Cole Turner <turner.cole@gmail.com>
2020-08-02 12:28:50 -0700

    add blog tags to sitemap

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -1,8 +1,11 @@
 // pages/sitemap.xml.js
 import { BASE_URL } from '../lib/constants';
-import { getAllPostsForSitemap } from '../lib/api';
+import { getAllPostsForSitemap, getAllTagsForSitemap } from '../lib/api';
 
-const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
+const createSitemap = ({
+  posts,
+  tags,
+}) => `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9" xmlns:news="http://www.google.com/schemas/sitemap-news/0.9" xmlns:xhtml="http://www.w3.org/1999/xhtml" xmlns:mobile="http://www.google.com/schemas/sitemap-mobile/1.0" xmlns:image="http://www.google.com/schemas/sitemap-image/1.1" xmlns:video="http://www.google.com/schemas/sitemap-video/1.1">
         <url>
             <loc>${`${BASE_URL}`}</loc>
@@ -19,6 +22,17 @@ const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
             <changefreq>daily</changefreq>
             <priority>0.5</priority>
         </url>
+        ${tags
+          .map(({ slug }) => {
+            return `
+                    <url>
+                        <loc>${`${BASE_URL}blog/${slug}`}</loc>
+                        <changefreq>daily</changefreq>
+                        <priority>0.7</priority>
+                    </url>
+                `;
+          })
+          .join('')}
         ${posts
           .map(({ slug }) => {
             return `
@@ -35,9 +49,10 @@ const createSitemap = (posts) => `<?xml version="1.0" encoding="UTF-8"?>
 
 export async function getServerSideProps({ res }) {
   const posts = await getAllPostsForSitemap();
+  const tags = await getAllTagsForSitemap();
 
   res.setHeader('Content-Type', 'text/xml');
-  res.write(createSitemap(posts));
+  res.write(createSitemap({ posts, tags }));
   res.end();
 
   return { props: {} };
