8e91c958ba0191e823ab2367221262d096c229e8
Cole Turner <turner.cole@gmail.com>
2020-08-27 02:46:23 -0700

    fix post image style

---
+++ .babelrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  env: {
+    development: {
+      presets: [
+        'next/babel',
+        [
+          require.resolve('@emotion/babel-preset-css-prop'),
+          {
+            sourceMap: true,
+            autoLabel: true,
+          },
+        ],
+      ],
+    },
+    production: {
+      presets: [
+        'next/babel',
+        [require.resolve('@emotion/babel-preset-css-prop'), { hoist: true }],
+      ],
+    },
+  },
+};
